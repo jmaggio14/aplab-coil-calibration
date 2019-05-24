d27aa605642fcfc4334b9218ca53910aee3dbf26
Jeff Maggio <jmaggio14@gmail.com>
2019-05-24 15:06:15 -0400

    updates and reorganization for Ruei

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 """
-This is an attempted recreation of Zhetuo's calibration map
+Capture and Calibration Procedure
+
+(will become more detailed and fleshed out as we develop hardware)
 
 
-Some assumption that are made:
+
+
+Assumptions:
     1) optitrack and coil data are perfectly synced. This should in theory be true
     because optitrack triggers coil data collection
 
@@ -12,7 +16,7 @@ COIL_FILENAME = None
 OPTITRACK_FILENAME = None
 BUTTER_ORDER = 3 # small is more gaussian, large is more ideal
 
-NOMINAL_12K_VOLTS = 1
+CALIBRATION_MAX_12K = 1
 NOMINAL_16K_VOLTS = 1
 NOMINAL_20K_VOLTS = 1
 
@@ -36,16 +40,18 @@ import argparse
 parser = argparse.ArgumentParser()
 parse.add_argument("--opti", default = None)
 parse.add_argument("--coil", default = None)
+
 parse.add_argument("--order", default = None)
 
-parse.add_argument("--nom_12k", default = None)
-parse.add_argument("--nom_16k", default = None)
-parse.add_argument("--nom_20k", default = None)
+parse.add_argument("--max-12k", default = None)
+parse.add_argument("--max-16k", default = None)
+parse.add_argument("--max-20k", default = None)
 
+parse.add_argument("--phase_delay1", default = None)
+parse.add_argument("--phase_delay2", default = None)
+parse.add_argument("--phase_delay3", default = None)
 
-parse.add_argument("--sys_phase1", default = None)
-parse.add_argument("--sys_phase2", default = None)
-parse.add_argument("--sys_phase3", default = None)
+parse.add_argument("--force-calibration", default = False)
 
 args = parser.parse_args()
 
@@ -58,15 +64,19 @@ if args.order:
     BUTTER_ORDER = args.order
 
 if args.nom_12k:
-    NOMINAL_12K_VOLTS = args.nom_12k
+    CALIBRATION_MAX_12K = args.nom_12k
 if args.nom_16k:
     NOMINAL_16K_VOLTS = args.nom_16k
 if args.nom_20k:
     NOMINAL_20K_VOLTS = args.nom_20k
 
 
-if args.sys_phase1:
-    SYS_PHASE_OFFSET_CH1
+if args.phase_delay1:
+    SYS_PHASE_OFFSET_CH1 = args.phase_delay1
+if args.phase_delay2:
+    SYS_PHASE_OFFSET_CH2 = args.phase_delay2
+if args.phase_delay2:
+    SYS_PHASE_OFFSET_CH2 = args.phase_delay2
 
 
 ############################# REAL CODE STARTS HERE ############################
@@ -80,9 +90,10 @@ optitrack_data = np.genfromtxt(OPTITRACK_FILENAME, skip_header=1, delimiter=",")
 SAMPLING_FREQUENCY = coil_data.shape[0] / coil_data[0][-1]
 
 # coil data is now an array shaped like the following
-# 1 | timestamp, coil1_Volts, coil2_Volts, coil3_Volts
-# 2 | timestamp, coil1_Volts, coil2_Volts, coil3_Volts
-# 3 | timestamp, coil1_Volts, coil2_Volts, coil3_Volts
+# all coil units in volts
+# 1 | timestamp, coil1, coil2, coil3, ref_coil1, ref_coil2, ref_coil3
+# 2 | timestamp, coil1, coil2, coil3, ref_coil1, ref_coil2, ref_coil3
+# 3 | timestamp, coil1, coil2, coil3, ref_coil1, ref_coil2, ref_coil3
 # ...
 
 # optirack data is now an array shaped like the following
@@ -100,7 +111,6 @@ SAMPLING_FREQUENCY = coil_data.shape[0] / coil_data[0][-1]
 # cutoff of desired +/- 1hz
 
 # construct a quick frequency filtering function for convienence
-# uses scipy.signal.get_window to create the profile
 def freq_filter(data, low, high, order, fs):
     """filters the data along axis=0 using butterworth filter to
     extract the correct frequency band
@@ -133,18 +143,19 @@ def freq_filter(data, low, high, order, fs):
 
     # calculate the frequency respone of the filter
     # check frequencies from 10K to 22K
-    freq_to_check = np.linspace(10e3, 22e3, 2)
+    freq_to_check = np.linspace(10e3, 22e3, 6000)
     w, h = signal.freqz(b, a, worN=freq_to_check, fs=fs)
-    return filtered
+    return filtered, (w, h)
 
 
 # remove the timestamps from the data
 channels = coil_data[:,1:]
 
 # apply our butterworth filter
-filtered_12k = freq_filter(channels, 12e3-1, 12e3+1, BUTTER_ORDER, SAMPLING_FREQUENCY)
-filtered_16k = freq_filter(channels, 16e3-1, 16e3+1, BUTTER_ORDER, SAMPLING_FREQUENCY)
-filtered_20k = freq_filter(channels, 20e3-1, 20e3+1, BUTTER_ORDER, SAMPLING_FREQUENCY)
+filtered_12k, _ = freq_filter(channels, 12e3-1, 12e3+1, BUTTER_ORDER, SAMPLING_FREQUENCY)
+filtered_16k, _ = freq_filter(channels, 16e3-1, 16e3+1, BUTTER_ORDER, SAMPLING_FREQUENCY)
+filtered_20k, _ = freq_filter(channels, 20e3-1, 20e3+1, BUTTER_ORDER, SAMPLING_FREQUENCY)
+
 # SAMPLES IS ROWS
 # COIL INDEX IS COLUMNS
 
@@ -152,11 +163,13 @@ filtered_20k = freq_filter(channels, 20e3-1, 20e3+1, BUTTER_ORDER, SAMPLING_FREQ
 ################################################################################
 # STEP 2
 # Amplitude calculation
-# retreive the Amplitude of the waveform using hilbert transformation
+# retreive the Amplitude of the waveform using a fourier transform
+#
+# maybe use analytical hilbert transformation?
 # see: https://www.gaussianwaves.com/2017/04/extracting-instantaneous-amplitude-phase-frequency-hilbert-transform/
 # and: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.hilbert.html
 
-# ANALYSICAL SIG TEST
+# ANALYSICAL SIG TEST - ignore
 # analytical_sig = signal.hilbert(filtered, axis=0)
 # amplitude = np.abs(analytical_sig)
 # phase = np.angle(analytical_sig)
@@ -173,6 +186,15 @@ phase_16k = np.angle(fft_16k)
 fft_20k = np.fft.fft(filtered_20k, axis=0)
 amplitude_20k = np.abs(fft_20k)
 phase_20k = np.angle(fft_20k)
+
+# break up measurement and reference coil data into separate arrays
+coil_amp_12k, ref_amp_12k = amplitude_12k[:,:3], amplitude_12k[:,3:]
+coil_amp_16k, ref_amp_16k = amplitude_16k[:,:3], amplitude_16k[:,3:]
+coil_amp_20k, ref_amp_20k = amplitude_20k[:,:3], amplitude_20k[:,3:]
+
+coil_phase_12k, ref_phase_12k = phase_12k[:,:3], phase_12k[:,3:]
+coil_phase_16k, ref_phase_16k = phase_16k[:,:3], phase_16k[:,3:]
+coil_phase_20k, ref_phase_20k = phase_20k[:,:3], phase_20k[:,3:]
 
 
 ################################################################################
@@ -209,49 +231,86 @@ assert upsampled_opti.shape[0] == amplitude.shape[0]
 
 
 ################################################################################
-# STEP 4
+# STEP 6
+# account for the phase offset inherent in system measurement
+# we do this using calibration data using a specified mount
+
+
+
+
+################################################################################
+# STEP 5
 # Calculate the rotation of each coil relative to each axis (nominal voltage)
 
-# sanity check/warning - see if the nominal voltage is smaller than the maximum
-# observed in the signal
-# 12 Khz
-max_12k = filtered_12k.max()
-max_16k = filtered_16k.max()
-max_20k = filtered_20k.max()
-if max_12k > NOMINAL_12K_VOLTS:
-    print("WARNING: 12Khz maximum ({}) is greater than the 12Khz Nominal ({})".format(max_12k, NOMINAL_12K_VOLTS))
-    print("WARNING: reseting 12K nominal to {}".format(max_12k))
-    NOMINAL_12K_VOLTS = max_12k
+# sanity check/warning - see if the calibration mins and maxes don't line up with
+# what was observed in the signal
+# find maxes
+coil_max_12k = coil_amp_12k.max()
+coil_max_16k = coil_amp_16k.max()
+coil_max_20k = coil_amp_20k.max()
+# find mins
+coil_min_12k = coil_amp_12k.min()
+coil_min_16k = coil_amp_16k.min()
+coil_min_20k = coil_amp_20k.min()
 
-if max_16k > NOMINAL_16K_VOLTS:
-    print("WARNING: 16Khz maximum ({}) is greater than the 16Khz Nominal ({})".format(max_16k, NOMINAL_16K_VOLTS))
-    print("WARNING: reseting 16K nominal to {}".format(max_16k))
-    NOMINAL_16K_VOLTS = max_16k
+# 12K
+if coil_max_12k > CALIBRATION_MAX_12K:
+    print("WARNING: observed 12Khz maximum ({}) is greater than the 12Khz Calibration Maximum ({})".format(coil_max_12k, CALIBRATION_MAX_12K))
+    print("WARNING: reseting 12K nominal to {}".format(coil_max_12k))
+    CALIBRATION_MAX_12K = coil_max_12k
+if coil_min_12k < CALIBRATION_MIN_12K:
+    print("WARNING: observed 12Khz minimum ({}) is greater than the 12Khz Calibration Minimum ({})".format(coil_min_12k, CALIBRATION_MIN_12K))
+    print("WARNING: reseting 12K minimum to {}".format(coil_min_12k))
+    CALIBRATION_MIN_12K = coil_min_12k
 
-if max_20k > NOMINAL_20K_VOLTS:
-    print("WARNING: 20Khz maximum ({}) is greater than the 20Khz Nominal ({})".format(max_20k, NOMINAL_20K_VOLTS))
-    print("WARNING: reseting 20K nominal to {}".format(max_20k))
-    NOMINAL_20K_VOLTS = max_20k
+# 16K
+if coil_max_16k > CALIBRATION_MAX_16K:
+    print("WARNING: observed 16Khz maximum ({}) is greater than the 16Khz Calibration Maximum ({})".format(coil_max_16k, CALIBRATION_MAX_16K))
+    print("WARNING: reseting 16K nominal to {}".format(coil_max_16k))
+    CALIBRATION_MAX_16K = coil_max_16k
+if coil_min_16k < CALIBRATION_MIN_16K:
+    print("WARNING: observed 16Khz minimum ({}) is greater than the 16Khz Calibration Minimum ({})".format(coil_min_16k, CALIBRATION_MIN_16K))
+    print("WARNING: reseting 16K minimum to {}".format(coil_min_16k))
+    CALIBRATION_MIN_16K = coil_min_16k
+
+# 20K
+if coil_max_20k > CALIBRATION_MAX_20K:
+    print("WARNING: observed 20Khz maximum ({}) is greater than the 20Khz Calibration Maximum ({})".format(coil_max_20k, CALIBRATION_MAX_20K))
+    print("WARNING: reseting 20K nominal to {}".format(coil_max_20k))
+    CALIBRATION_MAX_20K = coil_max_20k
+if coil_min_20k < CALIBRATION_MIN_20K:
+    print("WARNING: observed 20Khz minimum ({}) is greater than the 20Khz Calibration Minimum ({})".format(coil_min_20k, CALIBRATION_MIN_20K))
+    print("WARNING: reseting 20K minimum to {}".format(coil_min_20k))
+    CALIBRATION_MIN_20K = coil_min_20k
 
 
-    # where theta is such that parallel at theta=90, perpendicular at theta=0
-    # where nominal would be a calibration constant, collected when the coil is perfectly perpendicular
-    #
-    # coil_voltage = cos(theta) * nominal_voltage
-    # cos(theta) = coil_voltage / nominal_voltage | (in other words, we normalize it)
-    # theta = arccos( coil_voltage / nominal) | this gives us the abs(angle)
-    #
-    # we can use this to determine a normal vector component
+# where theta is such that parallel at theta=90, perpendicular at theta=0
+# where nominal would be a calibration constant, collected when the coil is perfectly perpendicular
+#
+# coil_voltage = cos(theta) * nominal_voltage
+# cos(theta) = coil_voltage / nominal_voltage | (in other words, we normalize it)
+# theta = arccos( coil_voltage / nominal) | this gives us the abs(angle)
+#
+# we can use this to determine a normal vector component
 
-# Divide by the nominal to normalize the data
-amplitude_12k = amplitude_12k / NOMINAL_12K_VOLTS
-amplitude_16k = amplitude_16k / NOMINAL_16K_VOLTS
-amplitude_20k = amplitude_20k / NOMINAL_20K_VOLTS
+# Normalize the amplitude
+coil_amp_12k = (coil_amp_12k - CALIBRATION_MIN_12K) / (CALIBRATION_MAX_12K - CALIBRATION_MIN_12K)
+coil_amp_16k = (coil_amp_16k - CALIBRATION_MIN_16K) / (CALIBRATION_MAX_16K - CALIBRATION_MIN_16K)
+coil_amp_20k = (coil_amp_20k - CALIBRATION_MIN_20K) / (CALIBRATION_MAX_20K - CALIBRATION_MIN_20K)
 
 # calculate each coils rotation relative to the field
-theta_12k = np.arccos( amplitude_12k )
-theta_16k = np.arccos( amplitude_16k )
-theta_20k = np.arccos( amplitude_20k )
+theta_12k = np.arccos( coil_amp_12k )
+theta_16k = np.arccos( coil_amp_16k )
+theta_20k = np.arccos( coil_amp_20k )
+
+
+
+
+
+
+
+
+
 
 
 
